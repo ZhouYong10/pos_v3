00796ac869b428119a5e434aafc9fb9f8f803fea
周勇 <1059981087@qq.com>
2015-01-08 16:26:04 +0800

    add PrivilegeRule

---
+++ src/models/PrivilegeRule.js
@@ -0,0 +1,9 @@
+/**
+ * Created by zhouyong on 15-1-8.
+ */
+function PrivilegeRule(id,category,ruleName,rule){
+    this.id = id;
+    this.category = category;
+    this.ruleName = ruleName;
+    this.rule = rule;
+}
