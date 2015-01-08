bf34df2bda9ea862f8552d53bac15003644dae4f
周勇 <1059981087@qq.com>
2015-01-08 16:56:56 +0800

    add method parse_bar_code_to_commodities in Cart

--- src/models/Cart.js
+++ src/models/Cart.js
@@ -4,3 +4,16 @@
 function Cart(barCodes){
     this.barCodes = barCodes;
 }
+
+Cart.prototype = {
+    parse_bar_codes_to_commodities : function(){
+        var barCodes = this.barCodes;
+        return _.map(barCodes,function(barCode){
+            for(var i in barCode){
+                var commodity = _.where(get_commodities(),{barCode:i});
+                commodity.num = barCode[i];
+                return commodity;
+            }
+        });
+    }
+};
