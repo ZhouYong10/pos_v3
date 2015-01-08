d1171e84e178d952e9f8e812d1a57b50e0589aac
周勇 <1059981087@qq.com>
2015-01-08 16:24:33 +0800

    add Commodity

---
+++ src/models/Commodity.js
@@ -0,0 +1,11 @@
+/**
+ * Created by zhouyong on 15-1-8.
+ */
+function Commodity(barCode,category,brand,commodityName,unit,price){
+    this.barCode = barCode;
+    this.category = category;
+    this.brand = brand;
+    this.commodityName = commodityName;
+    this.unit = unit;
+    this.price = price;
+}
