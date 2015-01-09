2a63344576ca7b4505e20332082aa9afb5caba7f
周勇 <1059981087@qq.com>
2015-01-09 10:50:27 +0800

    add attribute effective to PrivilegeRule

--- src/models/PrivilegeRule.js
+++ src/models/PrivilegeRule.js
@@ -1,8 +1,9 @@
 /**
  * Created by zhouyong on 15-1-8.
  */
-function PrivilegeRule(ruleName,category,rule){
+function PrivilegeRule(ruleName,category,rule,effective){
     this.ruleName = ruleName;
     this.category = category;
     this.rule = rule;
+    this.effective = effective?effective:'no';
 }
