79fb7c875848aa4e5bdb58926d7d98620ca88632
周勇 <1059981087@qq.com>
2015-01-10 17:46:34 +0800

    delete file PrivilegeRuleFactory

--- src/models/PrivilegeStrategy.js
+++ src/models/PrivilegeStrategy.js
@@ -1,7 +1,6 @@
 /**
  * Created by zhouyong on 15-1-8.
  */
-function PrivilegeStrategy(privilegeRules,allIsOn){
-    this.privilegeRules = privilegeRules;
-    this.allIsOn = allIsOn?allIsOn:false;
+function PrivilegeStrategy(){
+
 }
