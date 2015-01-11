351bbb26e1d76272ef5f7b91c61ed2687899f5d4
周勇 <1059981087@qq.com>
2015-01-11 21:28:51 +0800

    Add a strategic approach“

--- src/models/PrivilegeStrategy.js
+++ src/models/PrivilegeStrategy.js
@@ -1,6 +1,30 @@
 /**
  * Created by zhouyong on 15-1-8.
  */
-function PrivilegeStrategy(){
+var privilegeRuleFactory = new PrivilegeRuleFactory();
 
+function strategy_one(){
+    var itemOnSale = privilegeRuleFactory.item_on_sale(9.5);
+    var branOnSale = privilegeRuleFactory.brand_on_sale(9.0);
+    itemOnSale.relevance('可口可乐350Ml');
+    branOnSale.relevance('可口可乐');
+    itemOnSale.conflicts.push('brandOnSale-self');
+    branOnSale.conflicts.push('itemOnSale-other');
+
+}
+
+function strategy_two(){
+
+}
+
+function strategy_three(){
+
+}
+
+function strategy_four(){
+
+}
+
+function printInventory(input_barCodes){
+    
 }
