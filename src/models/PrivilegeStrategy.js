94063172fb6854aa6cda2710031db32343b4be0f
周勇 <1059981087@qq.com>
2015-01-11 21:53:48 +0800

    fix some bug

--- src/models/PrivilegeStrategy.js
+++ src/models/PrivilegeStrategy.js
@@ -5,20 +5,20 @@ var privilegeRuleFactory = new PrivilegeRuleFactory();
 
 function strategy_one(){
     var itemOnSale = privilegeRuleFactory.item_on_sale(9.5);
-    var branOnSale = privilegeRuleFactory.brand_on_sale(9.0);
-    itemOnSale.relevance('可口可乐350Ml');
-    branOnSale.relevance('可口可乐');
+    var brandOnSale = privilegeRuleFactory.brand_on_sale(9.0);
     itemOnSale.conflicts.push('brandOnSale-self');
-    branOnSale.conflicts.push('itemOnSale-other');
+    brandOnSale.conflicts.push('itemOnSale-other');
+    itemOnSale.relevance('可口可乐350Ml');
+    brandOnSale.relevance('可口可乐');
 }
 
 function strategy_two(){
     var itemOnSale = privilegeRuleFactory.item_on_sale(9.5);
-    var branOnSale = privilegeRuleFactory.brand_on_sale(9.0);
-    itemOnSale.relevance('可口可乐350Ml');
-    branOnSale.relevance('可口可乐');
+    var brandOnSale = privilegeRuleFactory.brand_on_sale(9.0);
     itemOnSale.conflicts.push('brandOnSale-other');
-    branOnSale.conflicts.push('itemOnSale-self');
+    brandOnSale.conflicts.push('itemOnSale-self');
+    itemOnSale.relevance('可口可乐350Ml');
+    brandOnSale.relevance('可口可乐');
     var itemOnReduce = privilegeRuleFactory.item_on_reduce('100-2');
     itemOnReduce.relevance('康师傅');
     itemOnReduce.rule = '100-5';
