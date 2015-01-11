4b27ad803344e9fda35793eb0b12c23c94942e03
周勇 <1059981087@qq.com>
2015-01-11 21:47:34 +0800

    add method strategy_two

--- src/models/PrivilegeStrategy.js
+++ src/models/PrivilegeStrategy.js
@@ -13,7 +13,16 @@ function strategy_one(){
 }
 
 function strategy_two(){
-
+    var itemOnSale = privilegeRuleFactory.item_on_sale(9.5);
+    var branOnSale = privilegeRuleFactory.brand_on_sale(9.0);
+    itemOnSale.relevance('可口可乐350Ml');
+    branOnSale.relevance('可口可乐');
+    itemOnSale.conflicts.push('brandOnSale-other');
+    branOnSale.conflicts.push('itemOnSale-self');
+    var itemOnReduce = privilegeRuleFactory.item_on_reduce('100-2');
+    itemOnReduce.relevance('康师傅');
+    itemOnReduce.rule = '100-5';
+    itemOnReduce.relevance('云山荔枝');
 }
 
 function strategy_three(){
